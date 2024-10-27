eeb5432326ec9e573454d7fdcddd0d1d02251fb4
Feros999 <ferospedrosa@gmail.com>
2024-10-27 08:55:33 +0700

    bmi.go

--- bmi.go
+++ bmi.go
@@ -5,15 +5,12 @@ import "fmt"
 func main() {
 	var bmi, tinggiBadan, beratBadan float64
 
-	// Meminta input BMI dan tinggi badan
 	fmt.Print("Masukkan nilai BMI: ")
 	fmt.Scan(&bmi)
 	fmt.Print("Masukkan tinggi badan (m): ")
 	fmt.Scan(&tinggiBadan)
-
-	// Menghitung berat badan berdasarkan rumus BMI = beratBadan / (tinggiBadan * tinggiBadan)
+	
 	beratBadan = bmi * (tinggiBadan * tinggiBadan)
 
-	// Menampilkan hasil berat badan
 	fmt.Printf("Berat badan anda adalah: %.f kg\n", beratBadan)
 }
